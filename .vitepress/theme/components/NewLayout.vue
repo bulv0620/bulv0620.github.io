937f2df6718961af8f9f2db5e5372d99a90db53b
bulv0620 <1241278815@qq.com>
2025-11-11 14:11:56 +0800

    update

--- .vitepress/theme/components/NewLayout.vue
+++ .vitepress/theme/components/NewLayout.vue
@@ -17,60 +17,60 @@
 import DefaultTheme from 'vitepress/theme'
 import Copyright from './Copyright.vue'
 import { withBase } from 'vitepress'
-import { onMounted } from 'vue'
+// import { onMounted } from 'vue'
 const { Layout } = DefaultTheme
 
-onMounted(() => {
-    const switchButton = document.querySelector('.VPSwitchAppearance')
-    window.addEventListener(
-        'click',
-        (e) => {
-            if (switchButton.contains(e.target)) {
-                e.stopImmediatePropagation() // 阻止 Vue 的 @click
-                e.preventDefault()
-                toggleTheme(e)
-            }
-        },
-        true // 捕获阶段
-    )
+// onMounted(() => {
+//     const switchButton = document.querySelector('.VPSwitchAppearance')
+//     window.addEventListener(
+//         'click',
+//         (e) => {
+//             if (switchButton.contains(e.target)) {
+//                 e.stopImmediatePropagation() // 阻止 Vue 的 @click
+//                 e.preventDefault()
+//                 toggleTheme(e)
+//             }
+//         },
+//         true // 捕获阶段
+//     )
 
-    function toggleTheme(e) {
-        const supportsViewTransition = !!document.startViewTransition
-        if (!supportsViewTransition) {
-            document.documentElement.classList.toggle('dark')
-            return
-        }
+//     function toggleTheme(e) {
+//         const supportsViewTransition = !!document.startViewTransition
+//         if (!supportsViewTransition) {
+//             document.documentElement.classList.toggle('dark')
+//             return
+//         }
 
-        const html = document.documentElement
+//         const html = document.documentElement
 
-        const transition = document.startViewTransition(() => {
-            document.documentElement.classList.toggle('dark')
-        })
+//         const transition = document.startViewTransition(() => {
+//             document.documentElement.classList.toggle('dark')
+//         })
 
-        transition.ready.then(() => {
-            const { clientX, clientY } = e
-            const radius = Math.hypot(Math.max(clientX, innerWidth - clientX), Math.max(clientY, innerHeight - clientY))
+//         transition.ready.then(() => {
+//             const { clientX, clientY } = e
+//             const radius = Math.hypot(Math.max(clientX, innerWidth - clientX), Math.max(clientY, innerHeight - clientY))
 
-            const clipPath = [
-                `circle(0% at ${clientX}px ${clientY}px)`,
-                `circle(${radius}px at ${clientX}px ${clientY}px)`
-            ]
+//             const clipPath = [
+//                 `circle(0% at ${clientX}px ${clientY}px)`,
+//                 `circle(${radius}px at ${clientX}px ${clientY}px)`
+//             ]
 
-            const isDarkNow = html.classList.contains('dark')
+//             const isDarkNow = html.classList.contains('dark')
 
-            document.documentElement.animate(
-                {
-                    clipPath: isDarkNow ? clipPath.reverse() : clipPath
-                },
-                {
-                    fill: 'both',
-                    duration: 500,
-                    pseudoElement: isDarkNow ? '::view-transition-old(root)' : '::view-transition-new(root)'
-                }
-            )
-        })
-    }
-})
+//             document.documentElement.animate(
+//                 {
+//                     clipPath: isDarkNow ? clipPath.reverse() : clipPath
+//                 },
+//                 {
+//                     fill: 'both',
+//                     duration: 500,
+//                     pseudoElement: isDarkNow ? '::view-transition-old(root)' : '::view-transition-new(root)'
+//                 }
+//             )
+//         })
+//     }
+// })
 </script>
 
 <style>
