87d8f2efcf8d660d8c6ed62e81021476764d0888
bulv0620 <1241278815@qq.com>
2026-01-28 16:22:00 +0800

    update

--- .vitepress/theme/components/NewLayout.vue
+++ .vitepress/theme/components/NewLayout.vue
@@ -4,7 +4,7 @@
             <div style="padding-top: 20px" class="post-info" v-if="!$frontmatter.page">
                 {{ $frontmatter.date?.substring(0, 10) }}
                 &nbsp;&nbsp;
-                <span class="tag" v-for="item in $frontmatter.tags" :key="item">
+                <span v-for="item in $frontmatter.tags" :key="item">
                     <a :href="withBase(`/pages/tags.html?tag=${item}`)"> {{ item }}</a>
                 </span>
             </div>
@@ -20,8 +20,19 @@ import { withBase } from 'vitepress'
 const { Layout } = DefaultTheme
 </script>
 
-<style>
-.tag {
+<style scoped>
+.post-info {
+    font-size: 12px;
+}
+
+.post-info span {
+    display: inline-block;
+    padding: 0 8px;
+    background-color: var(--vp-c-bg-alt);
+    margin-right: 6px;
     margin-bottom: 4px;
+    transition: 0.4s;
+    border-radius: 2px;
+    color: var(--vp-c-text-1);
 }
 </style>
